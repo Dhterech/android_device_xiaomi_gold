8eca866cd92a4152ed09d933b8ac9598244bf8f5
Dhterech <46405531+Dhterech@users.noreply.github.com>
2026-08-24 18:11:31 -0300

    Remove libaedv non existant soong namespace
    Change-Id: I52d4cb95f2d6cc7f1776d45b58594c225bbaf368

--- device.mk
+++ device.mk
@@ -258,9 +258,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/google/interfaces \
     hardware/mediatek \
-    hardware/mediatek/libaedv \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
+    #hardware/mediatek/libaedv \
 
 # Thermal
 PRODUCT_PACKAGES += \
